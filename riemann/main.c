69bdd326251f9f023be68bf48d3525dcdbee68e1
Matt Smith <archembaud@hotmail.com>
2025-12-15 15:59:51 +0800

    Complete 1D shock tube computation
    This change (i) fixed a bug in the left and right state computation
    prior to flux computation, (ii) properly updates state, and (iii)
    keeps track of time and breaks when the simulation is done. The
    computed results agree with theory; we are ready to move on.

--- riemann/main.c
+++ riemann/main.c
@@ -1,5 +1,7 @@
 #include "riemann.h"
 
+#define DEBUG 0
+
 // Thermodynamic constants
 #define GAMMA 1.4
 #define R 1.0
@@ -49,7 +51,7 @@ int main() {
 
     // Initialize the problem
     for (int i = 0; i < NO_CELLS; i++) {
-        if (i < NO_CELLS / 2) {
+        if (i < 0.5*NO_CELLS) {
             p0[i] = 10.0;   // Density
             p1[i] = 0.0;   // Velocity
             p2[i] = 1.0;   // Temperature
@@ -66,6 +68,7 @@ int main() {
     }
 
     // Start some time stepping
+    float time = 0.0;
     for (int timestep = 0; timestep < MAX_ITERATIONS; timestep++) {
         // Compute the maximum CFL condition
         double MAX_CFL = CPU_Compute_Max_CFL(p0, p1, p2, DX, DT, NO_CELLS);
@@ -104,33 +107,74 @@ int main() {
                 QR_cRT = sqrt(R * p2[NO_CELLS - 1]);
             } else {
                 // Normal case: take values from adjacent cells
-                QL_rho = p0[i];
-                QL_vx = p1[i];
+                QL_rho = p0[i-1];
+                QL_vx = p1[i-1];
                 QL_vy = 0.0;
                 QL_vz = 0.0;
-                QL_cRT = sqrt(R * p2[i]);
-                QR_rho = p0[i + 1];
-                QR_vx = p1[i + 1];
+                QL_cRT = sqrt(R * p2[i-1]);
+                QR_rho = p0[i];
+                QR_vx = p1[i];
                 QR_vy = 0.0;
                 QR_vz = 0.0;
-                QR_cRT = sqrt(R * p2[i + 1]);
+                QR_cRT = sqrt(R * p2[i]);
             }
 
             // Compute the flux at this interface
             double flux[5]; // The solver is a 3D solver, though we'll only use 1D here
             double interface_p[5];
+            if (DEBUG) {
+                printf("Computing flux at interface %d between cells %d and %d\n", i, i - 1, i);
+                printf("  Left state: rho = %g, vx = %g, T = %g\n", QL_rho, QL_vx, (QL_cRT * QL_cRT) / R);
+                printf("  Right state: rho = %g, vx = %g, T = %g\n", QR_rho, QR_vx, (QR_cRT * QR_cRT) / R);
+                printf("-------------------------------------------\n");
+            }
             CPU_Calc_Flux(flux, interface_p,
                 QL_rho, QL_vx, QL_vy, QL_vz, QL_cRT,
                 QR_rho, QR_vx, QR_vy, QR_vz, QR_cRT, R, GAMMA,
                 nx, ny, nz,
                 px, py, pz,
                 qx, qy, qz, 0); // wall_flag = 0; we handle the wall situation above
+            
+            // Store the fluxes of conserved variables
+            f0[i] = flux[0]; // Density flux
+            f1[i] = flux[1]; // Momentum flux
+            f2[i] = flux[4]; // Energy flux (note: flux[2] is y-momentum, flux[3] is z-momentum)
+
         } // End loop over interfaces
 
+        // Update the conserved variables in each cell using the fluxes
+        for (int i = 0; i < NO_CELLS; i++) {
+            if (DEBUG) printf("Upating cell %d: left mom flux = %g, right mom flux = %g\n", i, f1[i], f1[i + 1]);
+            u0[i] = u0[i] - (DT / DX) * (f0[i + 1] - f0[i]);
+            u1[i] = u1[i] - (DT / DX) * (f1[i + 1] - f1[i]);
+            u2[i] = u2[i] - (DT / DX) * (f2[i + 1] - f2[i]);
+        }           
 
+        // Update the primitive variables from the conserved variables
+        for (int i = 0; i < NO_CELLS; i++) {
+            p0[i] = u0[i]; // Density
+            p1[i] = u1[i] / u0[i]; // Velocity
+            double kinetic_energy = 0.5 * p1[i] * p1[i];
+            double total_energy_per_mass = u2[i] / u0[i];
+            double internal_energy_per_mass = total_energy_per_mass - kinetic_energy;
+            p2[i] = internal_energy_per_mass / CV; // Temperature   
+        }      
 
+        time += DT;
+        if (time >= FINAL_TIME) {
+            printf("Reached final time at timestep %d: time = %g\n", timestep, time);
+            break;
+        }
 
     }  // End time stepping
+
+    // Save the results
+    FILE *output_file = fopen("shock_tube_results.dat", "w");
+    for (int i = 0; i < NO_CELLS; i++) {
+        float x = (i + 0.5) * DX; // Cell center
+        fprintf(output_file, "%g\t%g\t%g\t%g\n", x, p0[i], p1[i], p2[i]);
+    }
+    fclose(output_file);
 
     return 0;
 }
