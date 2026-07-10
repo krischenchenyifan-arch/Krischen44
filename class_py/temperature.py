9705497b591c746867c60fc008e6e8f1f6a7cd76
Krischen <n16131015@ncku.edu.tw>
2026-07-10 15:08:50 +0800

    adjusted comment in code

--- class_py/temperature.py
+++ class_py/temperature.py
@@ -6,9 +6,9 @@ class Temperature:
     def from_celsius(cls, celsius):
         # 1. 先把攝氏換算成華氏
         fahrenheit = (9 / 5) * celsius + 32
-        # 2. 關鍵：用 cls(值) 來建立並回傳一個 Temperature 實體物件！
+        # 2. 關鍵：用 cls(值) 來建立並回傳一個 Temperature 實體物件
         return cls(fahrenheit)
 
-# 主程式（完全退回最左邊，沒有多餘懸空的裝飾器）
+# 主程式
 temp = Temperature.from_celsius(25)
-print(temp.fahrenheit)  # 預期輸出: 77.0
+print(temp.fahrenheit)
