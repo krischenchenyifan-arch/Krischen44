f88a2bd7e0ea6fc2d72f97f824aa06d4a49eb6ec
Krischen <n16131015@ncku.edu.tw>
2026-08-13 16:59:26 +0800

    I use for loop to optimize the code.

--- neural_network/dumb_advance1.py
+++ neural_network/dumb_advance1.py
@@ -30,8 +30,8 @@ hidden_weight_indices = [
 ]
 
 output_weight_indices = [
-    [20, 23, 26, 29, 32]
-    [21, 24, 27, 30, 33]
+    [20, 23, 26, 29, 32],
+    [21, 24, 27, 30, 33],
     [22, 25, 28, 31, 34]
 ]
 
@@ -47,70 +47,22 @@ class Neuron:
 	def sigmoid(a):
 		return 1/(1 + math.exp(-a))
 
+#Calculate hidden layer neurons' outputs
 hidden_output = []
-
-z4_0 = Neuron(input[0], weight[0])
-z4_1 = Neuron(input[1], weight[5])
-z4_2 = Neuron(input[2], weight[10])
-z4_3 = Neuron(input[3], weight[15])
-z4 = z4_0.output() + z4_1.output() + z4_2.output() + z4_3.output() + bias[4]
-z4_final = Neuron.sigmoid(z4)
-print(f"Neuron 4 output is {z4_final}")
-
-z5_0 = Neuron(input[0], weight[1])
-z5_1 = Neuron(input[1], weight[6])
-z5_2 = Neuron(input[2], weight[11])
-z5_3 = Neuron(input[3], weight[16])
-z5 = z5_0.output() + z5_1.output() + z5_2.output() + z5_3.output() + bias[5]
-z5_final = Neuron.sigmoid(z5)
-print(f"Neuron 5 output is {z5_final}")
-
-z6_0 = Neuron(input[0], weight[2])
-z6_1 = Neuron(input[1], weight[7])
-z6_2 = Neuron(input[2], weight[12])
-z6_3 = Neuron(input[3], weight[17])
-z6 = z6_0.output() + z6_1.output() + z6_2.output() + z6_3.output() + bias[6]
-z6_final = Neuron.sigmoid(z6)
-print(f"Neuron 6 output is {z6_final}")
-
-z7_0 = Neuron(input[0], weight[3])
-z7_1 = Neuron(input[1], weight[8])
-z7_2 = Neuron(input[2], weight[13])
-z7_3 = Neuron(input[3], weight[18])
-z7 = z7_0.output() + z7_1.output() + z7_2.output() + z7_3.output() + bias[7]
-z7_final = Neuron.sigmoid(z7)
-print(f"Neuron 7 output is {z7_final}")
-
-z8_0 = Neuron(input[0], weight[4])
-z8_1 = Neuron(input[1], weight[9])
-z8_2 = Neuron(input[2], weight[14])
-z8_3 = Neuron(input[3], weight[19])
-z8 = z8_0.output() + z8_1.output() + z8_2.output() + z8_3.output() + bias[8]
-z8_final = Neuron.sigmoid(z8)
-print(f"Neuron 8 output is {z8_final}")
-
+for idx, indices in enumerate(hidden_weight_indices):
+	neuron_id = idx + 4
+	z = sum(Neuron(input[i], weight[w_idx]).output() for i, w_idx in enumerate(indices)) + bias[neuron_id]
+	final_value = Neuron.sigmoid(z)
+	hidden_output.append(final_value)
+	print(f"Neuron{neuron_id} output is {final_value}")	
+print(hidden_output)
 print(f"======start calculate output layer outputs======")
 
-z9_4 = Neuron(z4_final, weight[20])
-z9_5 = Neuron(z5_final, weight[23])
-z9_6 = Neuron(z6_final, weight[26])
-z9_7 = Neuron(z7_final, weight[29])
-z9_8 = Neuron(z8_final, weight[32])
-z9 = z9_4.output() + z9_5.output() + z9_6.output() + z9_7.output() + z9_8.output() + bias[9]
-print(f"Neuron 9 output is {Neuron.sigmoid(z9)}")
-
-z10_4 = Neuron(z4_final, weight[21])
-z10_5 = Neuron(z5_final, weight[24])
-z10_6 = Neuron(z6_final, weight[27])
-z10_7 = Neuron(z7_final, weight[30])
-z10_8 = Neuron(z8_final, weight[33])
-z10 = z10_4.output() + z10_5.output() + z10_6.output() + z10_7.output() + z10_8.output() + bias[10]
-print(f"Neuron 10 output is {Neuron.sigmoid(z10)}")
-
-z11_4 = Neuron(z4_final, weight[22])
-z11_5 = Neuron(z5_final, weight[25])
-z11_6 = Neuron(z6_final, weight[28])
-z11_7 = Neuron(z7_final, weight[31])
-z11_8 = Neuron(z8_final, weight[34])
-z11 = z11_4.output() + z11_5.output() + z11_6.output() + z11_7.output() + z11_8.output() + bias[11]
-print(f"Neuron 11 output is {Neuron.sigmoid(z11)}")
+final_output = []
+for idx, indices in enumerate(output_weight_indices):
+	neuron_id = idx + 9
+	z = sum(Neuron(hidden_output[i], weight[w_idx]).output() for i,w_idx in enumerate(indices)) + bias[neuron_id]
+	final_value = Neuron.sigmoid(z)
+	final_output.append(final_value)
+	print(f"Neuron{neuron_id} output is {final_value}")
+print(final_output)
